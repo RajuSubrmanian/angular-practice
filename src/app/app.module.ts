eeac9a42117d4f49808102d8b6e4294a9e3df843
RajuSubrmanian <rajucse45@gmail.com>
2019-12-06 15:01:15 +0530

    Removed code pulling error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { SidebarComponentComponent } from './sidebar-component/sidebar-component.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, SidebarComponentComponent ],
+  declarations: [ AppComponent, HelloComponent, SidebarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
